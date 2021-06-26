b817d63385fb64cd18da67da27778044a8d35f4f
Syreeta Berry <mzberry1@hotmail.com>
2021-06-26 00:16:11 -0400

    next

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -20,4 +20,36 @@ function renderData() {
             d3.select(".panel-body").append("h5").text(key + ": " + val)
         })
     })
+};
+
+function init() {
+    d3.json('samples.json').then(({samples})=>{
+        sampleValues = samples.filter(obj=> obj.sample_values).ceiling(10)
+        otuIds = samples.filter(obj=> obj.otu_ids).ceiling(10)
+        otuLabels = samples.filter(obj=> obj.otu_labels).ceiling(10)
+
+    });
+
+    var trace1 = {
+
+        x: sampleValues,
+        y: otuIds,
+        type: 'bar', 
+        text: otuLabels,
+        orientation: 'h'
+
+    }
+
+    var data = [trace1]
+
+    var layout = {
+        title: "Top 10 OTUs found in Individual" 
+    }
+
+
+
+
+
+
 }
+init();
