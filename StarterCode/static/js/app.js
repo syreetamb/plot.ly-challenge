0348073f2a06e51dd7d7bb89a7e125d06933c45f
Syreeta Berry <mzberry1@hotmail.com>
2021-06-30 00:04:53 -0400

    done

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,4 @@
-d3.json("samples.json").then(({names})=>{
+d3.json("samples.json").then(({ names }) => {
     names.forEach(name => {
         d3.select('select').append('option').text(name)
     });
@@ -6,63 +6,73 @@ d3.json("samples.json").then(({names})=>{
 });
 
 function optionChanged() {
-  renderData();  
+    renderData();
 };
 
 function renderData() {
     var sel = d3.select('select').node().value;
     d3.select(".panel-body").html("");
-    d3.json('samples.json').then(({metadata,samples})=>{
-        metadata = metadata.filter(obj=> obj.id == sel)[0]
-        samples = samples.filter(obj=> obj.id == sel)[0]
-        sampleValues = samples.filter(obj=> obj.id == sel).map(row => row.sample_values)[0].slice(0,10)
-        otuIds = samples.filter(obj=> obj.otu_ids == sel)[0].slice(0,10)
-        otuLabels = samples.filter(obj=> obj.otu_labels == sel)[0].slice(0,10)
+    d3.json('samples.json').then(({ metadata, samples }) => {
+        metadata = metadata.filter(obj => obj.id == sel)[0]
+        samples = samples.filter(obj => obj.id == sel)[0]
 
-        Object.entries(metadata).forEach(([key, val])=>{
+        Object.entries(metadata).forEach(([key, val]) => {
             d3.select(".panel-body").append("h5").text(key + ": " + val)
-        })
-
+        });
 
         var trace1 = {
-
-            x: sampleValues,
-            y: otuIds,
-            type: 'bar', 
-            text: otuLabels,
+            x: samples.sample_values.slice(0, 10).reverse(),
+            y: samples.otu_ids.slice(0, 10).reverse().map(id => "OTU " + id),
+            type: 'bar',
+            text: samples.otu_labels.slice(0, 10).reverse(),
             orientation: 'h'
-    
         }
-    
+
         var data = [trace1]
-    
+
         var layout = {
-            title: "Top 10 OTUs Found On Each Individual" 
+            title: "Top 10 OTUs Found On Each Individual"
         }
-    
+
         Plotly.newPlot("bar", data, layout)
 
         var trace2 = {
-            x: otuIds,
-            y: sampleValues,
-            text: otuLabels,
+            x: samples.otu_ids,
+            y: samples.sample_values,
+            text: samples.otu_labels,
             mode: 'markers',
             marker: {
-              color: otuIds,
-              size: sampleValues
+                color: samples.otu_ids, 
+                size: samples.sample_values
             }
-          };
-          
+        };
+
         var data = [trace2];
-          
+
         var layout2 = {
             title: 'Top 10 OTUs Found On Each Individual',
             showlegend: false,
-        
+
         };
-          
+
         Plotly.newPlot("bubble", data, layout2);
 
+
+        
+        var data = [
+            {
+                domain: { x: [0,1], y: [0,1] },
+                value: metadata.wfreq,
+                title: { text: "Washing Frequency" },
+                type: "indicator",
+                mode: "gauge+number",
+                gauge:{axis:{range:[0,9]}, bar:{color: 'red'}}
+            }
+        ];
+        
+        var layout = { width: 600, height: 500, margin: { t: 0, b: 0 } };
+        Plotly.newPlot("gauge", data, layout);
+                
     })
 };
 
